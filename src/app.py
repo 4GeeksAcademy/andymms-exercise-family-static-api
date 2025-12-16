3f619fd846b575a0c14e33f7c0fe35e60f4740d4
Andymms <andreamm0714@gmail.com>
2025-12-16 15:21:28 +0000

    Proyecto Finalizado

--- src/app.py
+++ src/app.py
@@ -22,22 +22,55 @@ jackson_family = FamilyStructure("Jackson")
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-
 # Generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-
 @app.route('/members', methods=['GET'])
-def handle_hello():
-    # This is how you can use the Family datastructure by calling its methods
+def get_all_members():
     members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
+    response_body = members
     return jsonify(response_body), 200
 
+@app.route('/members', methods=['POST'])
+def add_member():
+    body = request.get_json()
+    if body is None:
+        return jsonify({'msg': 'Body is empty'}), 400
+    if 'first_name' not in body:
+        return jsonify({'msg': 'First name is required'}), 400
+    if 'age' not in body:
+        return jsonify({'msg': 'Age is required'}), 400
+    if 'lucky_numbers' not in body:
+        return jsonify({'msg': 'Lucky numbers is required'}), 400
 
+    new_member = {
+        'first_name': body['first_name'],
+        'last_name': jackson_family.last_name,
+        'age': body['age'],
+        'lucky_numbers': body['lucky_numbers']
+    }
+
+    if 'id' in body:
+        new_member['id'] = body['id']
+
+    jackson_family.add_member(new_member)
+    return jsonify(new_member), 200
+
+@app.route('/members/<int:id>', methods=['GET'])
+def get_one_member(id):
+    member = jackson_family.get_member(id)
+    if member:
+        return jsonify(member), 200
+    return jsonify({'msg': 'Member not found'}), 404
+
+@app.route('/members/<int:id>', methods=['DELETE'])
+def delete_one_member(id):
+    member = jackson_family.delete_member(id)
+    if member == True:
+        return jsonify({'done': True}), 200
+    return jsonify({'msg': 'Member not found'}), 404
 
 # This only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
